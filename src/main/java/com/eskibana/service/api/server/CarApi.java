848759fb9379026f3c313a1f2eadff73a4d2597b
wasinaphat-tig <wasinaphat.lil@truedigital.com>
2021-06-13 15:41:14 +0700

    create endpoint of elasticsearch

--- src/main/java/com/eskibana/service/api/server/CarApi.java
+++ src/main/java/com/eskibana/service/api/server/CarApi.java
@@ -1,6 +1,7 @@
 package com.eskibana.service.api.server;
 
 import com.eskibana.service.entity.Car;
+import com.eskibana.service.repository.CarElasticRepository;
 import com.eskibana.service.service.CarService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,10 @@ import java.util.concurrent.ThreadLocalRandom;
 public class CarApi {
 
     public static final Logger LOG = LoggerFactory.getLogger(CarApi.class);
+
+    @Autowired
+    private CarElasticRepository carRepository;
+
 
     @Autowired
     private CarService carService;
@@ -41,4 +46,41 @@ public class CarApi {
         return result;
 
     }
+
+    @GetMapping(value = "/count")
+    public String countCar(){
+        return "There are : " +carRepository.count() + " cars";
+    }
+
+    @PostMapping(value = "",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String saveCar(@RequestBody Car car){
+        var id =  carRepository.save(car).getId();
+        return "Save with ID :" + id;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Car getCar(@PathVariable("id") String carId){
+        return carRepository.findById(carId).orElse(null);
+    }
+
+    @PutMapping(value = "/{id}")
+    public String updateCar(@PathVariable("id") String carId,@RequestBody Car updatedCar){
+        updatedCar.setId(carId);
+      var newCar=  carRepository.save(updatedCar);
+      return "Updated car with ID : " + newCar.getId();
+    }
+    @GetMapping(value = "/find-json",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Car> findCarsByBrandAndColor(@RequestBody Car car){
+    return carRepository.findByBrandAndColor(car.getBrand(),car.getColor());
+    }
+
+    @GetMapping(value = "/cars/{brand}/{color}")
+    public List<Car> findCarsByPath(@PathVariable String brand,@PathVariable String color){
+        return carRepository.findByBrandAndColor(brand,color);
+    }
+    @GetMapping(value = "/cars")
+    public List<Car> findCarsByParam(@RequestParam String brand,@RequestParam String color){
+        return carRepository.findByBrandAndColor(brand,color);
+    }
+
 }
--- src/main/java/com/eskibana/service/repository/CarElasticRepository.java
+++ src/main/java/com/eskibana/service/repository/CarElasticRepository.java
@@ -4,7 +4,11 @@ import com.eskibana.service.entity.Car;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CarElasticRepository extends ElasticsearchRepository<Car,String> {
+
+    public List<Car> findByBrandAndColor(String brand,String color);
 
 }
